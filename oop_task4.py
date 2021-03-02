c8e04d40ee2f54d75cbc35375d517ee7a2786a1e
elenaslavutina <elena.slavutina2013@gmail.com>
2021-03-02 21:31:25 +0300

    ver2

--- oop_task4.py
+++ oop_task4.py
@@ -22,27 +22,33 @@ class Car:
         self.color = color
         self.speed = speed
         self.is_police = is_police
+        self.max_speed = 180
        #self.state = car_states[0]
-    def speed_increace(self, ):
+
+    def speed_increace(self, interval ):
+        if self.speed <= self.max_speed:
+            self.speed = self.speed + interval
+        else:
+            print(f"Невозможно увеличить скорость на {interval}. Текущая скорость {self.speed}, а предельная {self.max_speed}")
+
+    def speed_decreace(self, interval ):
+        if self.speed >= interval:
+            self.speed = self.speed - interval
+        else:
+            print(f"Невозможно уменьшить скорость на {interval}. Текущая Скорость {self.speed}")
 
     def go(self, speed):
         if speed == 0:
-            print("Вы стоите. Чтобы поехать нажмите на газ")
+            print("Вы стоите. Чтобы поехать увеличте скорость")
             return
 
         self.state = car_states[1]
 
     def stop(self):
-        self.state = car_states[0]
+        self.speed = 0
 
-    def stop(self, TurnRight = True):
-        if TurnRight:
-            self.state = car_states[2]
-        else:
-            self.state = car_states[3]
+
 
     def show_speed(self):
-        return self.state
+        return self.speed
 
-
-# Доделывай сама TownCar, SportCar, WorkCar, PoliceCar.
